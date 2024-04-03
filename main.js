729f5a9abd274be8ae6ab3e6ea79d5dc8b893fb0
pepapz <81101732+pepapz@users.noreply.github.com>
2024-04-03 21:15:52 +0900

    240403

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+window.onload = function() {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    let listBox = document.querySelectorAll('.listBox > li');
+    let imgBox = document.querySelector('.imgBox');
+    let img = document.querySelector('.imgBox img');
+
+    for( let i = 0; i < listBox.length; i++) {
+        listBox[i].addEventListener('mouseover', () => {
+            img.src = `images/img${i}.png`;
+            gsap.set(imgBox, {scale: 0, opacity: 0, duration: .3}),
+            gsap.to(imgBox, {scale:1, opacity: 1, duration: .3})
+        })
+        listBox[i].addEventListener('mousemove', (e) => {
+            let imgBoxX = e.pageX + 20;
+            let imgBoxY = e.pageY - 20;
+            imgBox.style.left = `${imgBoxX}px`;
+            imgBox.style.top = `${imgBoxY}px`;
+        })
+        listBox[i].addEventListener('mouseout',() => {
+            gsap.to(imgBox, {scale:0, opacity:0, duration: .3})
+        })
+    }
+}
+
 
 let rotate = document.querySelectorAll('.rotate');
 
